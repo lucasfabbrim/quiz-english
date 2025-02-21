e433f0960ecb1460907a5e1a7e19256eda2d0986
Lucas Mendes <lucasmendescontact@gmail.com>
2025-02-21 14:08:26 -0300

    fix: eslint page quiz

--- src/app/(public)/quiz/page.tsx
+++ src/app/(public)/quiz/page.tsx
@@ -2,9 +2,15 @@
 
 import { useRouter } from "next/navigation"
 import { motion } from "framer-motion"
+import { Button } from "@/components/ui/button"
+import { ArrowLeft } from "lucide-react"
 
 export default function Quiz() {
   const router = useRouter()
+
+  const handleBackToHome = () => {
+    router.back();
+  }
 
   return (
     <main className="min-h-screen bg-gradient-to-br from-[#17153b] via-[#2e236c] to-[#433d8b] flex items-center justify-center p-4">
@@ -14,7 +20,14 @@ export default function Quiz() {
         transition={{ duration: 0.5 }}
         className="flex flex-col w-full max-w-xs"
       >
-        
+        <Button 
+          onClick={() => handleBackToHome()}
+          variant="outline"
+          className="max-w-sm w-max px-4 py-1 bg-white/10 text-zinc-400 justify-start flex items-center rounded-full"
+        >
+          <ArrowLeft size={18}/>
+          Back
+        </Button>
       </motion.div>
     </main>
   )
