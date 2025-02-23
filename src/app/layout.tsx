af367bdd560fc5a38649638d2f9a7149dafe405a
Lucas Mendes <lucasmendescontact@gmail.com>
2025-02-23 14:48:17 -0300

    feat: adicionando analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-[#1C1B22] flex items-center justify-center p-4">{children}</body>
+      <body className="bg-[#1C1B22] flex items-center justify-center p-4"><Analytics/>{children}</body>
     </html>
   );
 }
